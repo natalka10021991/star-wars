e4255bc4d16eecc574780f7d7a13de2e4ecddd15
Rustam <Mirzamagomedov.R@traceone.com>
2022-01-27 01:25:56 +0300

    add eslint and update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import PeoplePage from './pages/PeoplePage';
-import HomePage from './pages/HomePage';
-import PlanetsPage from './pages/PlanetsPage';
-import StarshipsPage from './pages/StarshipsPage';
-import Nav from './components/Nav/Nav';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import PeoplePage from "./pages/PeoplePage";
+import HomePage from "./pages/HomePage";
+import PlanetsPage from "./pages/PlanetsPage";
+import StarshipsPage from "./pages/StarshipsPage";
+import Nav from "./components/Nav/Nav";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='wrapper'>
+      <div className="wrapper">
         <h1>Star Wars</h1>
         <Nav />
-        <Route path='/' exact component={HomePage} />
-        <Route path='/people' exact component={PeoplePage} />
-        <Route path='/planets' exact component={PlanetsPage} />
-        <Route path='/starships' exact component={StarshipsPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/people" exact component={PeoplePage} />
+        <Route path="/planets" exact component={PlanetsPage} />
+        <Route path="/starships" exact component={StarshipsPage} />
       </div>
     </BrowserRouter>
   );
--- src/components/PeopleList/PeopleList.tsx
+++ src/components/PeopleList/PeopleList.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { List, Avatar, Button } from 'antd';
-import { IPerson } from '../../types';
-import { getImageUrl } from '../PersonInfo/utils';
+import React from "react";
+import { List, Avatar, Button } from "antd";
+import { IPerson } from "../../types";
+import { getImageUrl } from "../PersonInfo/utils";
 
 interface PeopleListProps {
   people: IPerson[];
   updatePersonInfo: (name: string) => void;
 }
 
-const PeopleList: React.VFC<PeopleListProps> = ({ people, updatePersonInfo }) => {
+const PeopleList: React.VFC<PeopleListProps> = ({
+  people,
+  updatePersonInfo,
+}) => {
   return (
     <List
-      itemLayout='horizontal'
+      itemLayout="horizontal"
       dataSource={people}
       renderItem={(item) => (
         <List.Item>
           <List.Item.Meta
             avatar={<Avatar src={getImageUrl(item.id)} />}
             title={
-              <Button type='text' block onClick={() => updatePersonInfo(item.id)}>
+              <Button
+                type="text"
+                block
+                onClick={() => updatePersonInfo(item.id)}
+              >
                 {item.name}
               </Button>
             }
